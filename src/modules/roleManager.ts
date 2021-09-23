6928c9efe5f7c254b8940c8d79808385678f5b63
alex <alexandruturcanu915@gmail.com>
2021-09-24 02:07:59 +0300

    Fixed issues. (Messages were not being fetched, Roles were not being updated, simplified things)

--- src/modules/roleManager.ts
+++ src/modules/roleManager.ts
@@ -1,11 +1,12 @@
-import { Channel, Guild, GuildMember, MessageReaction, Role, TextChannel, User } from "discord.js";
+import { Guild, GuildMember, Message, MessageReaction, Role, User } from "discord.js";
 import Client from "../Client";
 import { DefaultConfigs } from "../objects/roleManager";
+import { fetchMessages } from "./utils";
 
 const moduleName = "roleManager";
 
-function getConfig(client: Client, guildId: string): DefaultConfigs {
-  let config: DefaultConfigs | null = client.configs.getConfig<DefaultConfigs>(moduleName, guildId);
+async function getConfig(client: Client, guildId: string): Promise<DefaultConfigs> {
+  let config: DefaultConfigs | null = await client.configs.getConfig<DefaultConfigs>(moduleName, guildId);
 
   if (!config) {
     config = new DefaultConfigs();
@@ -15,24 +16,49 @@ function getConfig(client: Client, guildId: string): DefaultConfigs {
   return config;
 }
 
+async function alterMembersRoles(member: GuildMember, newMemberRoleId: string, memberRoleId: string) {
+  if (memberRoleId) {
+    const memberRole: Role | null = await member.guild.roles.fetch(memberRoleId);
+    if (memberRole) {
+      await member.roles.add(memberRole);
+    }
+  }
+
+  if (newMemberRoleId) {
+    const newMemberRole: Role | null = await member.guild.roles.fetch(newMemberRoleId);
+    if (newMemberRole) {
+      await member.roles.remove([newMemberRole]);
+    }
+  }
+}
+
 async function onReady(client: Client) {
-  let configs: { [guildId: string]: DefaultConfigs } = client.configs.getConfigs(moduleName);
+  let configs: { [guildId: string]: DefaultConfigs } = await client.configs.getConfigs(moduleName);
 
   for (const [guildId, config] of Object.entries(configs)) {
-    if (!config.reactionListeningChannel || config.reactionMessageIds.length <= 0) continue;
+    if (!config.reactionListeningChannel || !config.reactionMessageIds.length) continue;
+    const messages: Message[] | void = await fetchMessages(client, guildId, config.reactionListeningChannel, config.reactionMessageIds).catch(error => console.error(error));
+
+    if (!messages) continue;
     const guild: Guild | null = await client.guilds.fetch(guildId);
-    if (guild) {
-      const channel: Channel | null = await guild.channels.fetch(config.reactionListeningChannel);
-      if (channel && typeof channel === typeof TextChannel) {
-        for (const messageId of config.reactionMessageIds)
-          (channel as TextChannel).messages.fetch(messageId);
+    if (!guild) continue;
+
+    for (const message of messages) {
+      for (const reaction of message.reactions.cache.values()) {
+        await reaction.users.fetch();
+        for (const user of reaction.users.cache.values()) {
+          const member: GuildMember | null = await guild.members.fetch(user.id);
+          if (member) await alterMembersRoles(member, config.newUserRoleId, config.memberRoleId);
+        }
       }
+
+      await message.reactions.removeAll();
     }
   }
 }
 
 async function onMemberJoin(member: GuildMember) {
-  const config: DefaultConfigs = getConfig(member.client as Client, member.guild.id);
+  const config: DefaultConfigs = await getConfig(member.client as Client, member.guild.id);
 
   if (!config.newUserRoleId) return;
 
@@ -43,25 +69,15 @@ async function onMemberJoin(member: GuildMember) {
 }
 
 async function onReactionAdd(messageReaction: MessageReaction, user: User) {
+  if (user.bot) return;
   const guild: Guild | null = messageReaction.message.guild;
   if (!guild) return;
 
   const member: GuildMember | null = await guild.members.fetch(user.id);
   if (!member) { console.error("How the actual... did a user that is not in the guild react to a message?"); return; }
 
-  const config: DefaultConfigs = getConfig(messageReaction.client as Client, guild.id);
-
-  if (config.memberRoleId) {
-    const memberRole: Role | null = await guild.roles.fetch(config.memberRoleId);
-    if (memberRole)
-      try { await member.roles.add(memberRole, "Bot added."); } catch (error) { console.error(error); }
-  }
-
-  if (config.newUserRoleId) {
-    const newMemberRole: Role | null = await guild.roles.fetch(config.newUserRoleId);
-    if (newMemberRole)
-      try { await member.roles.remove([newMemberRole]); } catch (error) { console.error(error); }
-  }
+  const config: DefaultConfigs = await getConfig(messageReaction.client as Client, guild.id);
+  await alterMembersRoles(member, config.newUserRoleId, config.memberRoleId);
 }
 
 export { onMemberJoin, onReactionAdd, onReady };
