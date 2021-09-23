b9454cbe389700c5709494fbf24fae010f024e7b
alex <alexandruturcanu915@gmail.com>
2021-09-24 02:03:51 +0300

    Turned the preloading of messages into its own method.

--- src/listeners/eventManager/onReady.ts
+++ src/listeners/eventManager/onReady.ts
@@ -1,21 +1,15 @@
-import { TextChannel } from "discord.js";
 import Client from "../../Client";
 import { DefaultConfig } from "../../objects/eventManager";
 import { Listener } from "..";
+import { fetchMessages } from "../../modules/utils";
 
-async function listen(client: Client) {
-  const configs: { [guildId: string]: DefaultConfig } = client.configs.getConfigs<DefaultConfig>("eventManager");
+async function listener(client: Client) {
+  const configs: { [guildId: string]: DefaultConfig } = await client.configs.getConfigs<DefaultConfig>("eventManager");
 
-  for (const [_, config] of Object.entries(configs)) {
-    if (!config.listenerChannelId) continue;
-    const channel = await client.channels.fetch(config.listenerChannelId);
-
-    if (!channel) continue;
-
-    for (const messageId of config.events.map(event => event.messageId)) {
-      try { await (channel as TextChannel).messages.fetch(messageId) } catch (error) { console.error(error) };
-    }
+  for (const [guildId, config] of Object.entries(configs)) {
+    if (!config.listenerChannelId || !config.events.length) continue;
+    fetchMessages(client, guildId, config.listenerChannelId, config.events.map(event => event.messageId)).catch(error => console.error(error));
   }
 }
 
-export default new Listener((client: Client) => client.on("ready", async () => await listen(client)));
+export default new Listener((client: Client) => client.on("ready", async () => await listener(client)));
