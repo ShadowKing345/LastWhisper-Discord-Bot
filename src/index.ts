14bb6a6f2d584501cb212a207d2d07b71ee088b7
alex <alexandruturcanu915@gmail.com>
2021-09-24 02:05:16 +0300

    Made loading of configs async

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
 import { Intents } from "discord.js";
 import Client from "./Client";
 import commandsSetup from "./commands";
-import tasksSetup from "./tasks/tasks";
+import tasksSetup from "./tasks";
 import listenersSetup from "./listeners";
 import permissionsSetup from "./permissionManager";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const client: Client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] }, "./guildConfigs.json");
+const client: Client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 client.once("ready", () => {
   console.log("Ready!");
 });
 
 (async () => {
+  await client.configs.loadConfigs();
   await commandsSetup(client);
   await tasksSetup(client);
   await listenersSetup(client);
