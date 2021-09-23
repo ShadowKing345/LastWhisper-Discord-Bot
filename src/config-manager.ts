6967990d1146d57313dbb9bcacc0b99ada860898
alex <alexandruturcanu915@gmail.com>
2021-09-24 02:05:40 +0300

    Reworked to work with both a local json file and a db client such as mongodb.

--- src/config-manager.ts
+++ src/config-manager.ts
@@ -1,53 +1,88 @@
 import fs from "fs";
-import path from "path";
+import mongoose from "mongoose";
+import { GuildConfig } from "./schemas/GuildConfigs";
 
 export default class ConfigManager {
-  filePath: string;
   configs: { [guildId: string]: { [configName: string]: {} } };
+  dbUrl: string | undefined;
+  useDb: string | undefined;
+  filePath: string | undefined;
 
-  constructor(filePath: string) {
-    this.filePath = path.join(__dirname, filePath);
+  constructor() {
     this.configs = {}
+    this.useDb = process.env.USEDB;
+    this.dbUrl = process.env.DBURL;
+    this.filePath = process.env.FILEURL;
   }
 
-  loadConfigs() {
-    if (!fs.existsSync(this.filePath) || !fs.lstatSync(this.filePath)?.isFile()) return;
+  async loadConfigs() {
+    if (this.useDb && this.dbUrl) {
+      mongoose.connect(this.dbUrl).then(() => console.log("Connected to DB")).catch(error => console.error(error));
+    } else {
+      if (!this.filePath || !fs.existsSync(this.filePath) || !fs.lstatSync(this.filePath)?.isFile()) return;
 
-    try {
-      this.configs = JSON.parse(fs.readFileSync(this.filePath, "utf8"));
-    } catch (error) { console.error(error); }
+      try {
+        this.configs = JSON.parse(fs.readFileSync(this.filePath, "utf8"));
+      } catch (error) { console.error(error); }
+    }
   }
 
   saveConfigs() {
+    if (this.useDb) return;
+
+    if (!this.filePath) return;
     try {
       fs.writeFileSync(this.filePath, JSON.stringify(this.configs, null, 2));
     } catch (error) { console.error(error); }
   }
 
-  getConfig<T>(key: string, guildId: string | null): T | null {
+  async getConfig<T>(key: string, guildId: string | null): Promise<T | null> {
     if (!guildId) return null;
-    if (!(guildId in this.configs)) return null;
-    if (!(key in this.configs[guildId])) return null;
-    return this.configs[guildId][key] as T;
+
+    if (this.useDb) {
+      return (await GuildConfig.findOne({ guildId: guildId }))?.get(`configs.${key}`) as T | null;
+    }
+    else {
+      if (!(guildId in this.configs)) return null;
+      if (!(key in this.configs[guildId])) return null;
+      return this.configs[guildId][key] as T;
+    }
   }
 
-  getConfigs<T>(key: string): { [guildId: string]: T } {
+  async getConfigs<T>(key: string): Promise<{ [guildId: string]: T }> {
     const result: { [guildId: string]: T } = {}
 
-    Object.entries(this.configs)
-      .forEach(entry => {
-        const [k, v] = entry;
-        if (key in v) {
-          result[k] = v[key] as T;
-        }
-      });
+    if (this.useDb) {
+      const k = `configs.${key}`;
+      const guildsConfigs = await GuildConfig.find({ k: { "$exists": 1 } });
 
+      guildsConfigs.forEach(guildConfig => {
+        result[guildConfig.guildId] = guildConfig.get(k) as T
+      });
+    }
+    else {
+      Object.entries(this.configs)
+        .forEach(entry => {
+          const [k, v] = entry;
+          if (key in v) {
+            result[k] = v[key] as T;
+          }
+        });
+    }
     return result;
   }
 
-  setConfig(key: string, guildId: string | null, data: object): void {
+  async setConfig(key: string, guildId: string | null, data: {}) {
     if (!guildId) return;
-    if (!(guildId in this.configs)) this.configs[guildId] = {};
-    this.configs[guildId][key] = data;
+    if (this.useDb) {
+      const guildConfig = await GuildConfig.findOne({ guildId: guildId }) ?? new GuildConfig({ guildId: guildId });
+
+      guildConfig.set(`configs.${key}`, data);
+
+      await guildConfig.save();
+    } else {
+      if (!(guildId in this.configs)) this.configs[guildId] = {};
+      this.configs[guildId][key] = data;
+    }
   }
 }
