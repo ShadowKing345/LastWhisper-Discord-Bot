776044522191d9a8076e8bba8b1cc82a574ae71a
alex <alexandruturcanu915@gmail.com>
2021-09-26 20:45:31 +0300

    Removed

--- src/tasks/configs/saveLoop.ts
+++
@@ -1,9 +0,0 @@
-import Client from "../../Client";
-import { Task } from "..";
-
-async function loop(client: Client) {
-  if (!client.isReady()) return;
-  client.configs.saveConfigs();
-}
-
-export default new Task("eventManager_postMessageLoop", 432000, loop);
