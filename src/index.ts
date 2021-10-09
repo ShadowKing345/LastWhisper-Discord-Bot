018d41421ca2a84f8b6b68cdd2055d062c8dd24c
alex <alexandruturcanu915@gmail.com>
2021-10-09 16:19:06 +0300

    Altered to use new project structure.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { Intents } from "discord.js";
-import Client from "./Client";
-import commandsSetup from "./commands";
-import tasksSetup from "./tasks";
-import listenersSetup from "./listeners";
-import permissionsSetup from "./permissionManager";
+import {Intents} from "discord.js";
+import Client from "./classes/Client";
+import moduleSetup from "./modules";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 console.log(`Current Timezone is set to ${process.env.TZ}`);
@@ -12,14 +10,11 @@ console.log(`Current Timezone is set to ${process.env.TZ}`);
 const client: Client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 client.once("ready", () => {
-  console.log("Ready!");
+    console.log("Ready!");
 });
 
 (async () => {
-  await client.configs.loadConfigs();
-  await commandsSetup(client);
-  await tasksSetup(client);
-  await listenersSetup(client);
-  await permissionsSetup(client);
-  await client.login(process.env.TOKEN);
+    await client.configs.loadConfigs();
+    await moduleSetup(client);
+    await client.login(process.env.TOKEN);
 })()
