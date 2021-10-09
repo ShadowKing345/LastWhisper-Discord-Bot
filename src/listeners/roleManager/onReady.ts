0ccf4135a3863fd8786ac23485cece1b606523db
alex <alexandruturcanu915@gmail.com>
2021-10-09 16:18:16 +0300

    Moved to respective module files.

--- src/listeners/roleManager/onReady.ts
+++
@@ -1,5 +0,0 @@
-import { Listener } from "..";
-import Client from "../../Client";
-import { onReady } from "../../modules/roleManager";
-
-export default new Listener((client: Client) => client.on("ready", async () => await onReady(client)));
