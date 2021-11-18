f8913a42a948edd2f473f94cc116ecec013374d7
alex <alexandruturcanu915@gmail.com>
2021-11-18 16:09:38 +0300

    Made the EventManagerConfig to be a class instead.

--- src/objects/EventManager.ts
+++ src/objects/EventManager.ts
@@ -44,15 +44,26 @@ class Tags {
     }
 }
 
-interface EventManagerConfig {
+class EventManagerConfig {
     _id: string;
-    listenerChannelId: string;
-    postingChannelId: string;
-    delimiterCharacters: SchemaDefinitionProperty<string[]>;
-    tags: SchemaDefinitionProperty<Tags> | undefined;
-    dateTimeFormat: SchemaDefinitionProperty<[string]>;
+    listenerChannelId: string | null;
+    postingChannelId: string | null;
+    delimiterCharacters: [string, string] | SchemaDefinitionProperty<[string, string]>;
+    tags: Tags;
+    dateTimeFormat: string[];
     events: EventObj[];
     reminders: ReminderTrigger[];
+
+    constructor(id: string = "", listenerChannelId: string = null, postingChannelId: string = null, delimiterCharacters: [string, string] = ["\\[", "\\]"], tags: Tags = new Tags(), dateTimeFormat: string[] = [], events: EventObj[] = [], reminders: ReminderTrigger[] = []) {
+        this._id = id;
+        this.listenerChannelId = listenerChannelId;
+        this.postingChannelId = postingChannelId;
+        this.delimiterCharacters = delimiterCharacters;
+        this.tags = tags;
+        this.dateTimeFormat = dateTimeFormat;
+        this.events = events;
+        this.reminders = reminders;
+    }
 }
 
 export {ReminderTrigger, EventObj, EventManagerConfig, Tags};
