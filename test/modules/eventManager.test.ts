7c123e6272dfbc8854a796e5fcfe4a4a754e51ee
alex <alexandruturcanu915@gmail.com>
2021-12-01 17:48:48 +0300

    Added more tests.

--- test/modules/eventManager.test.ts
+++ test/modules/eventManager.test.ts
@@ -1,49 +1,50 @@
-import {getConfig, parseMessage, splitChunk} from "../../src/modules/eventManager"
-import {EventManagerConfig, EventObj, ReminderTrigger, Tags} from "../../src/objects/EventManager";
+import {createEvent, getConfig, parseMessage} from "../../src/modules/eventManager"
+import {EventManagerConfig, EventObj, Tags} from "../../src/objects/EventManager";
+import {Message, MessageReaction} from "discord.js";
 
 jest.mock("../../src/models/EventManager", () => {
-    let data = [
-        new class implements EventManagerConfig {
-            _id: string = "correct";
-            dateTimeFormat: string[] = [];
-            delimiterCharacters: [string, string] = ["\\[", "\\]"];
-            events: EventObj[] = [];
-            listenerChannelId: string | null = null;
-            postingChannelId: string | null = null;
-            reminders: ReminderTrigger[] = [];
-            tags: Tags = new class implements Tags {
-                announcement: string = "Event Announcement";
-                dateTime: string = "Time";
-                description: string = "Event Description";
-                exclusionList: string[] = ["Ignore"];
-            };
-        }
+    let data: EventManagerConfig[] = [
+        {
+            _id: "correct",
+            dateTimeFormat: [],
+            delimiterCharacters: ["\\[", "\\]"],
+            events: [],
+            listenerChannelId: "123",
+            postingChannelId: "123",
+            reminders: [],
+            tags: {
+                announcement: "Event Announcement",
+                dateTime: "Time",
+                description: "Event Description",
+                exclusionList: ["Ignore"],
+            } as Tags,
+        } as EventManagerConfig
     ]
 
     return ({
-        findOne: async (filter: { [key: string]: string }) => {
+        findOne: async (filter) => {
             for (let [key, value] of Object.entries(filter)) {
                 let found = data.find(entry => entry[key] && entry[key] === value);
-                if (found != null) return found;
+                if (found != null) {
+                    found["save"] = async function () {
+                    }
+                    return found;
+                }
             }
 
             return null;
         },
-        create: async (setters: { [key: string]: string }) => {
+        create: async (docs) => {
             let obj = new EventManagerConfig();
-            for (let [key, value] of Object.entries(setters)) {
+            for (let [key, value] of Object.entries(docs)) {
                 obj[key] = value;
             }
 
+            obj["save"] = async function () {
+            }
             return obj;
-        }
+        },
     });
-});
-
-describe("Chunk Splitting Method", () => {
-    it("Normal Expected Operation", () => expect(splitChunk(["Event Announcement", "Example Tittle", "Event Description", "Example Description", "Time", "Fish"])).toEqual([["Event Announcement", "Example Tittle"], ["Event Description", "Example Description"], ["Time", "Fish"]]));
-    it("Uneven Array", () => expect(splitChunk(["Test"])).toEqual([["Test", undefined]]));
-    it("Empty Array", () => expect(splitChunk([])).toEqual([]));
 });
 
 describe("Event Parser", () => {
@@ -51,16 +52,20 @@ describe("Event Parser", () => {
     config.tags.exclusionList.push("Ignore");
 
     describe("Valid Message", () => {
-        let content = "[ Event Announcement ]\n" +
-            "Example Title\n" +
-            "[ Event Description ]\n" +
-            "Example Description\n" +
-            "[ Example Tag 1 ]\n" +
-            "Test info!\n" +
-            "[ Ignore ]\n" +
-            "This should be ignored." +
-            "[ Time ]\n" +
-            "<t:1636976220:D>";
+        let content = `[ Event Announcement ]
+        Example Title
+        
+        [ Event Description ]
+        Example Description
+        
+        [ Example Tag 1 ]
+        Test info!
+        
+        [ Ignore ]
+        This should be ignored.
+        
+        [ Time ]
+        <t:1636976220:D>`;
         let result = parseMessage(null, content, config);
 
         it("Is Valid", () => expect(result.isValid).toBe(true));
@@ -71,6 +76,27 @@ describe("Event Parser", () => {
     it("No Valid Tags", () => expect(parseMessage(null, "yolo", config).isValid).toBe(false));
     it("Empty Paragraph", () => expect(parseMessage(null, "", config).isValid).toBe(false));
     it("Missing Tag Value", () => expect(parseMessage(null, "[Event Announcement]", config).isValid).toBe(false));
+    it("Null Message", () => expect(parseMessage(null, null, config).isValid).toBe(false));
+    it("Message has weird lines.", async () => {
+        let content = `@everyone
+            
+            [ Event Announcement ]
+            Example Title
+            
+            [ Event Description ]
+            Example Description
+            
+            [ Example Tag 1 ]
+            Test info!
+            
+            [ Ignore ]
+            This should be ignored.
+            
+            [ Time ]
+            <t:1636976220:D>`;
+
+        expect(parseMessage(null, content, config).isValid).toBe(true);
+    });
 });
 
 describe("Acquiring configs", () => {
@@ -81,28 +107,86 @@ describe("Acquiring configs", () => {
 });
 
 describe("Create Event", () => {
-   it.todo("Correct Message");
-   it.todo("Incorrect Message");
-   it.todo("Null Message");
+    let config: EventManagerConfig = new EventManagerConfig();
+    config.tags.exclusionList.push("Ignore");
+    let content = `[ Event Announcement ]
+        Example Title
+        
+        [ Event Description ]
+        Example Description
+        
+        [ Example Tag 1 ]
+        Test info!
+        
+        [ Ignore ]
+        This should be ignored.
+        
+        [ Time ]
+        <t:1636976220:D>`;
+    let message: Message;
+
+    beforeEach(() => {
+        message = {
+            author: {id: "1"},
+            client: {application: {id: "2"}},
+            id: "1234",
+            guildId: "correct",
+            content: content,
+            channelId: "123",
+            react: jest.fn() as (emoji) => Promise<MessageReaction>
+        } as Message;
+        jest.resetAllMocks();
+    });
+
+    it("Is Author Bot?", async () => {
+        message.author.id = "2";
+        await createEvent(message);
+        expect(message.react).not.toHaveBeenCalled();
+    });
+    it("GuildId Invalid", async () => {
+        message.guildId = "wrong id";
+        await createEvent(message);
+        expect(message.react).not.toHaveBeenCalled();
+    });
+
+    it("Correct Message", async () => {
+        console.log(message)
+        await createEvent(message);
+        expect(message.react).toHaveBeenCalledWith("✅");
+    });
+    it("Incorrect Announcement Tags / Null Message", async () => {
+        message.content = "";
+        await createEvent(message);
+        expect(message.react).not.toHaveBeenCalled();
+
+        message.content = null;
+        await createEvent(message);
+        expect(message.react).not.toHaveBeenCalled();
+    });
+    it("Incorrect Message", async () => {
+        message.content = "[ Event Announcement ]\nExample Title";
+        await createEvent(message);
+        expect(message.react).toHaveBeenCalledWith("❎");
+    });
 });
 
 describe("Update Event", () => {
-   it.todo("Correct Id, Correct Message");
-   it.todo("Correct Id, Incorrect Message");
-   it.todo("Correct Id, Null Message");
-   it.todo("Incorrect Id, Correct Message");
-   it.todo("Incorrect Id, Incorrect Message");
-   it.todo("Incorrect Id, Null Message");
+    it.todo("Correct Id, Correct Message");
+    it.todo("Correct Id, Incorrect Message");
+    it.todo("Correct Id, Null Message");
+    it.todo("Incorrect Id, Correct Message");
+    it.todo("Incorrect Id, Incorrect Message");
+    it.todo("Incorrect Id, Null Message");
 });
 
 describe("Delete Event", () => {
-   it.todo("Correct Id");
-   it.todo("Incorrect Id");
-   it.todo("Null Id");
+    it.todo("Correct Id");
+    it.todo("Incorrect Id");
+    it.todo("Null Id");
 });
 
 describe("Reminder Loop", () => {
-   it.todo("Work on testing idea.")
+    it.todo("Work on testing idea.")
 });
 
 describe("Event Command", () => {
