b3aa740367f32edf51e951e221f5af362978daf3
alex <alexandruturcanu915@gmail.com>
2021-12-11 19:39:45 +0300

    Began to add support to message components.

--- src/classes/Module.ts
+++ src/classes/Module.ts
@@ -1,17 +1,20 @@
 import Command from "./Command";
 import Listener from "./Listener";
 import Task from "./Task";
+import {BaseMessageComponent} from "discord.js";
 
 export class Module {
     name: string;
-    commands: Array<Command>;
-    listeners: Array<Listener>;
-    tasks: Array<Task>;
+    commands: Command[];
+    listeners: Listener[];
+    tasks: Task[];
+    actions: BaseMessageComponent[]
 
     constructor(name?: string) {
         this.name = name;
         this.commands = [];
         this.listeners = [];
         this.tasks = [];
+
     }
 }
