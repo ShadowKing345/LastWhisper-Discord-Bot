c968b57b6163c98e09c5dc42cd1b449735b8a7cf
alex <alexandruturcanu915@gmail.com>
2022-01-21 22:30:03 +0300

    Renamed to app.ts. Removed dotenv calls and got rid of the preInit method. Changed all environment calls to use app configs instead.

--- src/code/app.ts
+++ src/code/app.ts
@@ -1,32 +1,27 @@
 import {Intents} from "discord.js";
 import Client from "./classes/Client";
 import moduleSetup from "./modules";
-import dotenv from "dotenv";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 import weekOfYear from "dayjs/plugin/weekOfYear";
 import advancedFormat from "dayjs/plugin/advancedFormat";
 import customParseFormat from "dayjs/plugin/customParseFormat";
+import {getConfigs} from "./configurations/appConfigs";
+import ConfigManager from "./configManager";
 
-export function preInit() {
-    dotenv.config();
+dayjs.extend(duration);
+dayjs.extend(weekOfYear);
+dayjs.extend(advancedFormat);
+dayjs.extend(customParseFormat);
 
-    dayjs.extend(duration);
-    dayjs.extend(weekOfYear);
-    dayjs.extend(advancedFormat);
-    dayjs.extend(customParseFormat);
-}
-
-preInit();
-console.log(`Current Timezone is set to ${process.env.TZ}`);
-
+const appConfigs = getConfigs();
 const client: Client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]});
 
 client.once("ready", () => {
-    console.log("Ready!");
+    console.log("Discord Bot is now loaded and ready to be used!");
 });
 
-client.configs.loadConfigs()
+ConfigManager.establishDBConnection(appConfigs)
     .then(() => moduleSetup(client))
-    .then(() => client.login(process.env.TOKEN))
+    .then(() => client.login(appConfigs.getToken))
     .catch(err => console.error(err));
