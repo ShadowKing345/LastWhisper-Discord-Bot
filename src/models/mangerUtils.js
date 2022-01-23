b6ee53c500fc66e4bf6563ce71acb2a42108fcbe
alex <alexandruturcanu915@gmail.com>
2022-01-23 21:43:16 +0300

    how did that happen?

--- src/models/mangerUtils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ManagerUtilsConfig = void 0;
-class ManagerUtilsConfig {
-}
-exports.ManagerUtilsConfig = ManagerUtilsConfig;
--- src/models/roleManager.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoleManagerConfig = void 0;
-class RoleManagerConfig {
-}
-exports.RoleManagerConfig = RoleManagerConfig;
