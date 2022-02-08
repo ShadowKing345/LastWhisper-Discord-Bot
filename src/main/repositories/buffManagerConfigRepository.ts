4a4d2e099fbb7688fd21f746fa25e3baf2286416
alex <alexandruturcanu915@gmail.com>
2022-02-08 09:31:01 +0300

    ensured that the data acquired from the db is actually the object with methods instead of a basic data object. (only for buffManager)

--- src/main/repositories/buffManagerConfigRepository.ts
+++ src/main/repositories/buffManagerConfigRepository.ts
@@ -1,12 +1,13 @@
 import {Collection, Filter} from "mongodb";
 import {DB} from "../config/databaseConfiguration.js";
-import {BuffManagerConfig} from "../models/buffManager.js";
+import {BuffManagerConfig, Buff, Days, MessageSettings, Week} from "../models/buffManager.js";
 
 export class BuffManagerConfigRepository {
     private static readonly collectionName: string = "buff_manager";
     private collection: Collection<BuffManagerConfig>
 
-    constructor() {}
+    constructor() {
+    }
 
     private async validate() {
         if (!this.collection) this.collection = await DB.collection(BuffManagerConfigRepository.collectionName);
@@ -16,16 +17,29 @@ export class BuffManagerConfigRepository {
         await this.validate();
         let result = await this.collection.findOneAndReplace({guildId: config.guildId}, config, {upsert: true});
 
-        return result.ok ? config : null;
+        return result.ok ? this.sanitiseOutput(config) : null;
     }
 
     public async findOne(filter: Filter<BuffManagerConfig>): Promise<BuffManagerConfig> {
         await this.validate();
-        return await this.collection.findOne(filter);
+        return this.sanitiseOutput(await this.collection.findOne(filter));
     }
 
-    public async find(filter: Filter<BuffManagerConfig>): Promise<BuffManagerConfig[]>{
+    public async find(filter: Filter<BuffManagerConfig>): Promise<BuffManagerConfig[]> {
         await this.validate();
-        return await this.collection.find(filter).toArray();
+        return (await this.collection.find(filter).toArray()).map(config => this.sanitiseOutput(config));
+    }
+
+    private sanitiseOutput(config: BuffManagerConfig): BuffManagerConfig {
+        config = Object.assign(new BuffManagerConfig(), config);
+        config.messageSettings = Object.assign(new MessageSettings(), config.messageSettings);
+        config.buffs = config.buffs.map(day => Object.assign(new Buff(), day));
+        config.weeks = config.weeks.map(week => {
+            week = Object.assign(new Week(), week);
+            week.days = Object.assign(new Days(), week.days);
+            return week;
+        });
+
+        return config;
     }
 }
