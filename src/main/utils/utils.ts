889957f7a33b0b40c11be837da7aa7aea18baf38
alex <alexandruturcanu915@gmail.com>
2022-02-26 16:31:30 +0300

    - Got rid of the DaysToArray method. (finally) - Made export keywords inline.

--- src/main/utils/utils.ts
+++ src/main/utils/utils.ts
@@ -1,7 +1,6 @@
 import {Channel, Client, Guild, Message, Snowflake, TextChannel} from "discord.js";
-import {Days} from "../models/buffManager.model.js";
 
-async function fetchMessages(client: Client, guildId: Snowflake, channelId: Snowflake, messageIds: Snowflake[]): Promise<Message[]> {
+export async function fetchMessages(client: Client, guildId: Snowflake, channelId: Snowflake, messageIds: Snowflake[]): Promise<Message[]> {
     const result: Message[] = [];
 
     if (!client.guilds.cache.has(guildId)) return;
@@ -20,9 +19,3 @@ async function fetchMessages(client: Client, guildId: Snowflake, channelId: Snow
 
     return result;
 }
-
-function DaysToArray(days: Days): string[] {
-    return [days.sunday, days.monday, days.tuesday, days.wednesday, days.thursday, days.friday, days.saturday]
-}
-
-export {fetchMessages, DaysToArray}
