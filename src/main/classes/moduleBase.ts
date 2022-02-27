c6f525614449284bb2f7bb79435100f44f881ac5
alex <alexandruturcanu915@gmail.com>
2022-02-27 14:13:11 +0300

    Reduced to be public calls.

--- src/main/classes/moduleBase.ts
+++ src/main/classes/moduleBase.ts
@@ -3,29 +3,8 @@ import {Listener} from "./listener.js";
 import {Task} from "./task.js";
 
 export class ModuleBase {
-    protected _moduleName = "";
-    protected _command?: Command;
-    protected _commands: Command[] = [];
-    protected _listeners: Listener[] = [];
-    protected _tasks: Task[] = [];
-
-    get moduleName(): string {
-        return this._moduleName;
-    }
-
-    get command(): Command {
-        return this._command;
-    }
-
-    get commands(): Command[] {
-        return this._commands;
-    }
-
-    get listeners(): Listener[] {
-        return this._listeners;
-    }
-
-    get tasks(): Task[] {
-        return this._tasks;
-    }
+    public moduleName = "";
+    public commands: Command[] = [];
+    public listeners: Listener[] = [];
+    public tasks: Task[] = [];
 }
