2d6c17095ad4fe51cb2ec69fc4cbe2371ff799cf
alex <alexandruturcanu915@gmail.com>
2022-02-27 14:12:57 +0300

    Added method to get the built command from a Command object.

--- src/main/classes/command.ts
+++ src/main/classes/command.ts
@@ -5,3 +5,15 @@ export class Command {
     public command: Partial<SlashCommandBuilder | ((builder: SlashCommandBuilder) => SlashCommandBuilder)>;
     public run: (interaction: CommandInteraction) => Promise<void>;
 }
+
+export function BuildCommand(command: Command): SlashCommandBuilder {
+    if (!command.command) {
+        return null;
+    }
+
+    if (command.command instanceof SlashCommandBuilder) {
+        return command.command;
+    } else if (typeof command.command === "function") {
+        return (command.command(new SlashCommandBuilder()));
+    }
+}
