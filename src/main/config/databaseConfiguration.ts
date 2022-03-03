8cfc1174b2e09fbf05f265449e591f09b96ef14e
alex <alexandruturcanu915@gmail.com>
2022-03-03 13:08:51 +0300

    Improved the way client is connected to. Url building is not stable atm. Registered the client and Database to the DI container.

--- src/main/config/databaseConfiguration.ts
+++ src/main/config/databaseConfiguration.ts
@@ -1,14 +1,29 @@
 import {Db, MongoClient} from "mongodb";
 import {CONFIGS} from "./appConfigs.js";
 import {logger} from "../utils/logger.js";
+import {container} from "tsyringe";
 
-export let DB: Db;
+export class Database extends Db {}
 
-export async function createDbClient() {
-    const client = await MongoClient.connect(CONFIGS.dbUrl);
-    DB = client.db();
-    client.on("error", error => {
-        logger.error(error.message, {context: "DatabaseConfiguration"});
-        client.close();
-    })
+export let CLIENT: MongoClient;
+export let DB: Database;
+
+export async function connectClient(): Promise<MongoClient> {
+    const url = CONFIGS.database?.url ?? `mongodb+srv://${CONFIGS.database.username}:${CONFIGS.database.password}@${CONFIGS.database.host}${CONFIGS.database.port ? ":" + CONFIGS.database.port : ""}/${CONFIGS.database.database}?${CONFIGS.database.query}`;
+
+    if (!CLIENT) {
+        CLIENT = await MongoClient.connect(url);
+        CLIENT.on("error", error => {
+            logger.error(error.message, {context: "DatabaseConfiguration"});
+            CLIENT.close();
+        });
+        container.register<MongoClient>(MongoClient, {useValue: CLIENT});
+    }
+
+    if (!DB) {
+        DB = CLIENT.db(CONFIGS.database.database);
+        container.register<Database>(Database, {useValue: DB});
+    }
+
+    return CLIENT;
 }
