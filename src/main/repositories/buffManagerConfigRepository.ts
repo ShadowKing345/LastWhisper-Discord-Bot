39bcca1fa95f727929497864f14a4cbfbcb736c2
alex <alexandruturcanu915@gmail.com>
2022-03-03 13:06:42 +0300

    Inverted control for the database object for the buff manager repo.

--- src/main/repositories/buffManagerConfigRepository.ts
+++ src/main/repositories/buffManagerConfigRepository.ts
@@ -1,31 +1,28 @@
 import {Collection, Filter} from "mongodb";
-import {DB} from "../config/databaseConfiguration.js";
+import {Database} from "../config/databaseConfiguration.js";
 import {Buff, BuffManagerConfig, Days, MessageSettings, Week} from "../models/buffManager.model.js";
 import {injectable} from "tsyringe";
 
 @injectable()
 export class BuffManagerConfigRepository {
-    private static readonly collectionName: string = "buff_manager";
+    private readonly collectionName: string = "buff_manager";
     private collection: Collection<BuffManagerConfig>
 
-    private async validate() {
-        if (!this.collection) this.collection = await DB.collection(BuffManagerConfigRepository.collectionName);
+    constructor(private db: Database) {
+        this.collection = db.collection(this.collectionName);
     }
 
     public async save(config: BuffManagerConfig): Promise<BuffManagerConfig> {
-        await this.validate();
         const result = await this.collection.findOneAndReplace({guildId: config.guildId}, config, {upsert: true});
 
         return result.ok ? this.sanitiseOutput(config) : null;
     }
 
     public async findOne(filter: Filter<BuffManagerConfig>): Promise<BuffManagerConfig> {
-        await this.validate();
         return this.sanitiseOutput(await this.collection.findOne(filter));
     }
 
     public async find(filter: Filter<BuffManagerConfig>): Promise<BuffManagerConfig[]> {
-        await this.validate();
         return (await this.collection.find(filter).toArray()).map(config => this.sanitiseOutput(config));
     }
 
