fe7dd194f35469029a83673a655925ca2980cc36
alex <alexandruturcanu915@gmail.com>
2022-03-21 20:39:19 +0300

    added ts-jest configs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,15 @@
-module.exports = {
-    displayName: "FFXIV Discord Bot Jest",
+const ts = require("ts-jest/jest-preset");
+const mongoDb = require("@shelf/jest-mongodb/jest-preset");
+const {recursive} = require("merge");
+
+module.exports = recursive(ts, mongoDb, {
     notify: true,
     testRegex: "(/test[s]/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
-    preset: "@shelf/jest-mongodb",
-    extensionsToTreatAsEsm: [".ts"],
-};
+    bail: false,
+    globals: {
+        "ts-jest": {
+            useESM: true,
+            isolatedModules: true
+        }
+    }
+});
