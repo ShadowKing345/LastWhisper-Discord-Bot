4fcf7588cc6629b2e6cd18ab92bb923a910ad73e
alex <alexandruturcanu915@gmail.com>
2022-03-21 17:23:27 +0300

    Added jest mongo db.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
-  displayName: "FFXIV Discord Bot Jest",
-  notify: true,
-  testRegex: "(/test[s]/.*|(\\.|/)(test|spec))\\.[jt]sx?$"
+    displayName: "FFXIV Discord Bot Jest",
+    notify: true,
+    testRegex: "(/test[s]/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+    preset: "@shelf/jest-mongodb",
+    extensionsToTreatAsEsm: [".ts"],
 };
