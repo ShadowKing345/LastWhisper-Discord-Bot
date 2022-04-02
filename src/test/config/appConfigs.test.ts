338091e63b72748802b4bb71d087d78facb99b55
alex <alexandruturcanu915@gmail.com>
2022-04-02 11:56:02 +0300

    Added more tests.

--- src/test/config/appConfigs.test.ts
+++ src/test/config/appConfigs.test.ts
@@ -1,14 +1,30 @@
-import { jest } from "@jest/globals";
-import { initConfigs } from "../../main/config/appConfigs";
+import {jest} from "@jest/globals";
+import {AppConfigs, DatabaseConfiguration, parseConfigFile} from "../../main/config/appConfigs";
+import {vol} from "memfs";
 
-describe("App Configuration", function () {
-    it("should be able to open and read content of a file.", function () {
-        console.log(initConfigs());
+jest.mock("fs");
 
-        expect(true).toBe(true);
+const config: AppConfigs = new AppConfigs();
+config.token = "Fish";
+config.database = new DatabaseConfiguration();
+
+describe("App Configuration", () => {
+    beforeEach(() => vol.reset());
+
+    it("should be able to parse an existing file", () => {
+        vol.fromJSON({
+            "./appConfigs.json": JSON.stringify(config),
+        });
+        expect(parseConfigFile()).toEqual(config);
+    });
+
+    it('should return null if no file was found', () => {
+        expect(parseConfigFile()).not.toBeTruthy();
     });
 
     afterAll(() => {
+        vol.reset();
         jest.clearAllMocks();
-    })
+    });
 });
+
