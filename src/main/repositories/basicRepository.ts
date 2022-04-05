358ab419725dc1fa89b659f570a015769b3fe47e
alex <alexandruturcanu915@gmail.com>
2022-04-06 00:58:20 +0300

    Fixed issue with warning and type mismatches.

--- src/main/repositories/basicRepository.ts
+++ src/main/repositories/basicRepository.ts
@@ -1,19 +1,23 @@
 import { Collection, Filter } from "mongodb";
 
-export abstract class BasicRepository<T> {
+import { BasicModel } from "../models/basicModel.js";
+
+export abstract class BasicRepository<T extends BasicModel> {
     protected collection: Collection<T>;
 
     public async save(config: T): Promise<T> {
-        const result = await this.collection.findOneAndReplace(config, { upsert: true });
-        return this.sanitiseOutput(result);
+        const result = await this.collection.findOneAndReplace({ _id: config._id }, config, { upsert: true });
+        return result.ok ? this.sanitiseOutput(result.value as T) : null;
     }
 
     public async findOne(filter: Filter<T>): Promise<T> {
-        return this.sanitiseOutput(await this.collection.findOne(filter));
+        // For some reason due to the void call Webstorm is having issues seeing this method call.
+        // noinspection JSVoidFunctionReturnValueUsed
+        return this.sanitiseOutput(await this.collection.findOne(filter) as T);
     }
 
     public async find(filter: Filter<T>): Promise<T[]> {
-        return (await this.collection.find(filter).toArray()).map(config => this.sanitiseOutput(config));
+        return (await this.collection.find(filter).toArray()).map(config => this.sanitiseOutput(config as T));
     }
 
     public async bulkSave(configs: T[]) {
