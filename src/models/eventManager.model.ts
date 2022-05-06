cc3a207eb33457bf31126c69d6f12e6280a26fbb
alex <alexandruturcanu915@gmail.com>
2022-05-06 13:08:10 +0300

    Added default.

--- src/models/eventManager.model.ts
+++ src/models/eventManager.model.ts
@@ -1,8 +1,8 @@
-import {BasicModel} from "./basicModel.js";
+import { BasicModel } from "./basicModel.js";
 
 export class ReminderTrigger {
-    public message: string;
-    public timeDelta: string;
+    public message: string = null;
+    public timeDelta: string = null;
 
     constructor(message: string, timeDelta: string) {
         this.message = message;
@@ -11,11 +11,11 @@ export class ReminderTrigger {
 }
 
 export class EventObj {
-    public messageId: string;
-    public name: string;
-    public description: string;
-    public dateTime: number;
-    public additional: [string, string][];
+    public messageId: string = null;
+    public name: string = null;
+    public description: string = null;
+    public dateTime: number = null;
+    public additional: [string, string][] = [];
 
     constructor(messageId: string, name = "", description = "", dateTime: number = null, additional: [string, string][] = []) {
         this.messageId = messageId;
@@ -25,16 +25,16 @@ export class EventObj {
         this.additional = additional;
     }
 
-    get isValid(): boolean {
-        return this.name != "" && this.description != "" && this.dateTime != null;
+    static isValid(obj: EventObj): boolean {
+        return obj.name != "" && obj.description != "" && obj.dateTime != null;
     }
 }
 
 export class Tags {
-    public announcement: string;
-    public description: string;
-    public dateTime: string;
-    public exclusionList: string[];
+    public announcement: string = null;
+    public description: string = null;
+    public dateTime: string = null;
+    public exclusionList: string[] = [];
 
     constructor(announcement = "Event Announcement", description = "Event Description", dateTime = "Time", exclusionList: string[] = []) {
         this.announcement = announcement;
@@ -45,12 +45,12 @@ export class Tags {
 }
 
 export class EventManagerConfig extends BasicModel {
-    public guildId: string;
-    public listenerChannelId: string | null;
-    public postingChannelId: string | null;
-    public delimiterCharacters: [string, string];
-    public tags: Tags;
-    public dateTimeFormat: string;
-    public events: EventObj[];
-    public reminders: ReminderTrigger[];
+    public guildId: string = null;
+    public listenerChannelId: string | null = null;
+    public postingChannelId: string | null = null;
+    public delimiterCharacters: [string, string] = ["[", "]"];
+    public tags: Tags = new Tags();
+    public dateTimeFormat: string = null;
+    public events: EventObj[] = [];
+    public reminders: ReminderTrigger[] = [];
 }
