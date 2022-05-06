ed85fbb4f4ad42ffd841d7ee015269cf1ba917ec
alex <alexandruturcanu915@gmail.com>
2022-05-06 13:10:12 +0300

    Renamed to init.

--- src/app.ts
+++ src/app.ts
@@ -13,23 +13,27 @@ export class App {
         this.client = new Client();
     }
 
-    public async load() {
-        logger.info("Loading Configurations", { context: "ClientSetup" });
+    public async init() {
+        logger.info("Loading Configurations", {context: "ClientSetup"});
         initConfigs();
         if (CONFIGS.logging_level) {
             logger.level = CONFIGS.logging_level;
         }
 
-        logger.info("Creating Db Client", { context: "ClientSetup" });
+        logger.info("Creating Db Client", {context: "ClientSetup"});
         await connectClient();
 
-        logger.info("Loading modules.", { context: "ClientSetup" });
+        logger.info("Loading modules.", {context: "ClientSetup"});
         configureModules(this.client);
 
-        this.client.once("ready", () => {logger.info(chalk.magentaBright("Bot is up and ready to roll!"), { context: "ClientRuntime" });});
-        this.client.on("error", error => {logger.info(`${error.name}: ${error.message}`, { context: "ClientRuntime" });});
+        this.client.once("ready", () => {
+            logger.info(chalk.magentaBright("Bot is up and ready to roll!"), {context: "ClientRuntime"});
+        });
+        this.client.on("error", error => {
+            logger.info(`${error.name}: ${error.message}`, {context: "ClientRuntime"});
+        });
 
-        logger.log("info", `Done loading. ${chalk.green("Ready to run.")}`, { context: "ClientSetup" });
+        logger.log("info", `Done loading. ${chalk.green("Ready to run.")}`, {context: "ClientSetup"});
     }
 
     public async run() {
@@ -39,7 +43,7 @@ export class App {
 
 export async function botMain() {
     const app: App = new App();
-    await app.load();
+    await app.init();
 
     await app.run();
 }
