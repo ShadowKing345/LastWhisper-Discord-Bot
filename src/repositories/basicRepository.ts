d781a27e669d212d5aae0288edf2f6d13074e2c8
alex <alexandruturcanu915@gmail.com>
2022-06-05 17:44:47 +0300

    Added interface for repository and added more functions to basic repository.

--- src/repositories/basicRepository.ts
+++ src/repositories/basicRepository.ts
@@ -2,7 +2,17 @@ import { Collection, Filter } from "mongodb";
 
 import { BasicModel } from "../models/basicModel.js";
 
-export abstract class BasicRepository<T extends BasicModel> {
+export interface IRepository<T> {
+    findOne(filter: Filter<T>): Promise<T>;
+
+    find(filter: Filter<T>): Promise<T[]>;
+
+    save(obj: T): Promise<T>;
+
+    bulkSave(config: T[]): Promise<void>;
+}
+
+export abstract class BasicRepository<T extends BasicModel> implements IRepository<T> {
     protected collection: Collection<T>;
 
     public async save(config: T): Promise<T> {
@@ -18,6 +28,10 @@ export abstract class BasicRepository<T extends BasicModel> {
 
     public async find(filter: Filter<T>): Promise<T[]> {
         return (await this.collection.find(filter).toArray()).map(config => this.sanitiseOutput(config as T));
+    }
+
+    public getAll(): Promise<T[]> {
+        return this.find({});
     }
 
     public async bulkSave(configs: T[]) {
