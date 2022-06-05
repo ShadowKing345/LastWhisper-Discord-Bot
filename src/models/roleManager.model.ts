d14f836d35eaa15acd91c635c2d25bee8b54708f
alex <alexandruturcanu915@gmail.com>
2022-06-05 17:46:49 +0300

    Changed how role assignment is handled.

--- src/models/roleManager.model.ts
+++ src/models/roleManager.model.ts
@@ -2,8 +2,7 @@ import { BasicModel } from "./basicModel.js";
 
 export class RoleManagerConfig extends BasicModel {
     public guildId: string;
-    public newUserRoleId: string;
-    public memberRoleId: string;
+    public acceptedRole: string;
     public reactionMessageIds: string[];
     public reactionListeningChannel: string;
 }
