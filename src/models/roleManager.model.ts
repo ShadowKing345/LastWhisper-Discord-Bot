f41a11c73ed593178cdddbb87666f6066afe4a26
alex <alexandruturcanu915@gmail.com>
2022-06-06 14:24:47 +0300

    Changed name to make more sense.

--- src/models/roleManager.model.ts
+++ src/models/roleManager.model.ts
@@ -2,7 +2,7 @@ import { BasicModel } from "./basicModel.js";
 
 export class RoleManagerConfig extends BasicModel {
     public guildId: string;
-    public acceptedRole: string;
+    public acceptedRoleId: string;
     public reactionMessageIds: string[];
     public reactionListeningChannel: string;
 }
