ac3ebcf91610633b66aa1166e7d66a5b52007e2c
alex <alexandruturcanu915@gmail.com>
2022-06-13 18:14:59 +0300

    allowed to just chain strings instead of having to create the string each time.

--- src/permission_manager/authorize.decorator.ts
+++ src/permission_manager/authorize.decorator.ts
@@ -1,11 +1,11 @@
 import { CommandInteraction } from "discord.js";
 
-export function authorize(key: string) {
+export function authorize(...key: string[]) {
     return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
         const originalValue = descriptor.value;
 
         descriptor.value = async function (interaction: CommandInteraction, ...args: any[]) {
-            if (!await this.permissionManager.isAuthorized(interaction, key)) {
+            if (!await this.permissionManager.isAuthorized(interaction, key.join("."))) {
                 return interaction.reply({
                     content: "Sorry you do not have the permissions to use this command.",
                     ephemeral: true,
