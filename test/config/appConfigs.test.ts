61f6c31462e17862ac2b7056f17e765fc8091f6d
alex <alexandruturcanu915@gmail.com>
2022-07-12 16:26:31 +0300

    Fixed tests.

--- test/config/appConfigs.test.ts
+++ test/config/appConfigs.test.ts
@@ -1,16 +1,42 @@
+import "reflect-metadata";
+
 import { test } from "tap";
 
-import { AppConfigs } from "../../src/config/app_configs/index.js";
+import { AppConfig, configPath, devConfigPath, parseConfigFile } from "../../src/config/app_configs/index.js";
 
 test("appConfigs.ts", t => {
-    t.test("Config file parsing", t => {
+    t.test("Config file parsing.", t => {
         const dir = t.testdir({
-            "./appConfigs.json": JSON.stringify(new AppConfigs()),
+            [`${configPath}`]: JSON.stringify(new AppConfig()),
         });
 
-        const result = AppConfigs.parseConfigFile(`${dir}/appConfigs.json`, `${dir}/appConfigs=dev.json`);
+        const result = parseConfigFile(`${dir}/${configPath}`, `${dir}/${devConfigPath}`);
         t.ok(result);
         t.end();
     });
+
+    t.test("Dev config file parsing.", t => {
+        const config = new AppConfig();
+        config.token = "config";
+
+        const devConfig = new AppConfig();
+        devConfig.token = "devConfig";
+
+        const dir = t.testdir({
+            [`${configPath}`]: JSON.stringify(config),
+            [`${devConfigPath}`]: JSON.stringify(devConfig),
+        });
+
+        const result = parseConfigFile(`${dir}/${configPath}`, `${dir}/${devConfigPath}`);
+        t.equal(result.token, "devConfig");
+        t.end();
+    });
+
+    t.test("Returns null when file not found.", t => {
+        const dir = t.testdir({});
+        t.notOk(parseConfigFile(`${dir}/${configPath}`));
+        t.end();
+    });
+
     t.end();
 });
