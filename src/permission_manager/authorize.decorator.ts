d619bfefbeec171891954e2bd377f9460512a754
alex <alexandruturcanu915@gmail.com>
2022-07-15 12:20:01 +0300

    Made decorator try to resolve an instance of the service. Or use the one inside the class if possible.

--- src/permission_manager/authorize.decorator.ts
+++ src/permission_manager/authorize.decorator.ts
@@ -1,11 +1,15 @@
 import { CommandInteraction } from "discord.js";
+import { container } from "tsyringe";
+
+import { PermissionManagerService } from "./permissionManager.service.js";
 
 export function authorize(...key: string[]) {
     return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+        const permissionManager = target.permissionManager ?? container.resolve(PermissionManagerService);
         const originalValue = descriptor.value;
 
         descriptor.value = async function (interaction: CommandInteraction, ...args: any[]) {
-            if (!await this.permissionManager.isAuthorized(interaction, key.join("."))) {
+            if (!await permissionManager.isAuthorized(interaction, key.join("."))) {
                 return interaction.reply({
                     content: "Sorry you do not have the permissions to use this command.",
                     ephemeral: true,
