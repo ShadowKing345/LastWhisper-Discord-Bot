7ce9ef20910e2d0d590afebea8ac85978357cab0
alex <alexandruturcanu915@gmail.com>
2022-07-28 15:31:31 +0300

    Removed tap and implemented jest.

---
+++ test/jest.test.ts
@@ -0,0 +1,24 @@
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+describe("Should be able to", () => {
+    describe("do basic function tests such as", () => {
+        test("1 + 1 = 2", () => {
+            expect(sum(1, 1)).toEqual(2);
+        });
+
+        test("1 + 2 != 2", () => {
+            expect(sum(1, 2)).not.toEqual(2);
+        });
+    });
+
+    // test("mock now.", async t => {
+    //     // const clock = useFakeTimers();
+    //     // const now = DateTime.now().toUnixInteger();
+    //     // t.same(now, DateTime.now().toUnixInteger(), "Should be same time.");
+    //     //
+    //     // clock.restore();
+    //     // t.end();
+    // });
+});
