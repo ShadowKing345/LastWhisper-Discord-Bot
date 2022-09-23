d94ef2fc4fc54485eb78a826406710454acf7c4c
alex <alexandruturcanu915@gmail.com>
2022-09-23 13:36:27 +0300

    Renamed File.

--- src/config/app_configs/models/projectConfiguration.model.ts
+++ src/config/app_configs/models/projectConfiguration.model.ts
@@ -1,7 +1,7 @@
 import { CommandRegistrationConfiguration } from "./commandRegistrationConfiguration.model.js";
 import { DatabaseConfiguration } from "./databaseConfiguration.model.js";
 
-export class AppConfig {
+export class ProjectConfiguration {
     public token?: string = null;
     public database?: DatabaseConfiguration = new DatabaseConfiguration();
     public logging_level?: string = "info";
