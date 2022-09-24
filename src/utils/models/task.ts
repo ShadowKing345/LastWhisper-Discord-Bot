acd5e253cff3475539d931bf776d646081c01c54
alex <alexandruturcanu915@gmail.com>
2022-09-24 20:18:53 +0300

    Disabled this functionality for now.

--- src/utils/models/task.ts
+++ src/utils/models/task.ts
@@ -5,16 +5,16 @@ import { LoggerFactory } from "../logger/logger.js";
 import { Client } from "./client.js";
 
 export class Task {
-    private static readonly logger = container.resolve(LoggerFactory).buildLogger(Task.name);
+    // private static readonly logger = container.resolve(LoggerFactory).buildLogger(Task.name);
     public name: string;
     public timeout: number;
     public run: (client: Client) => Promise<void>;
 
     public static async waitTillReady(client: DiscordClient, checkAgainTime = 500) {
-        Task.logger.debug(`Waiting for client to be ready.`);
+        // Task.logger.debug(`Waiting for client to be ready.`);
         while (!client.isReady()) {
             await new Promise(resolve => setTimeout(resolve, checkAgainTime));
         }
-        Task.logger.debug(`Client is ready.`);
+        // Task.logger.debug(`Client is ready.`);
     }
 }
