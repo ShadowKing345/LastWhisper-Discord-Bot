a9cdcd721542faf1511a6f9dfbf4e50ea8f032ae
alex <alexandruturcanu915@gmail.com>
2022-10-07 12:07:45 +0300

    Moved key collection to service file.

---
+++ src/modules/index.ts
@@ -0,0 +1,8 @@
+// Index file to be used purely to have the files be loaded.
+
+import "./buffManager.module.js";
+import "./eventManager.module.js";
+import "./gardeningManager.module.js";
+import "./managerUtils.module.js";
+import "./permissionManager.module.js";
+import "./roleManager.module.js";
--- src/utils/decorators/addCommandKeys.ts
+++ src/utils/decorators/addCommandKeys.ts
@@ -1,11 +1,10 @@
 import { PermissionKeys } from "../../services/permissionManager.service.js";
-import { PermissionKeysType } from "../../models/permission_manager/index.js";
 
 /**
  * Decorator that adds a list of keys to a permission keys list.
  */
 export function addCommandKeys() {
-    return function (target: PermissionKeysType, key: string | symbol) {
+    return function (target: any, key: string | symbol) {
         const value = target[key];
         PermissionKeys.push(value);
         return Object(value);
