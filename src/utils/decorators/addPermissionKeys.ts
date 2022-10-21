3f4b54d79e06897421991e58f1b53a21ff716128
alex <alexandruturcanu915@gmail.com>
2022-10-21 16:02:15 +0300

    Added decoration to automatically add permission keys.

---
+++ src/utils/decorators/addPermissionKeys.ts
@@ -0,0 +1,15 @@
+import { flattenObject } from "../index.js";
+import { PermissionManagerService } from "../../services/permissionManager.service.js";
+
+/**
+ * Adds an object of values into the permission service keys list.
+ * Note that the object will get flattened down to a depth of 1 then the values are processed.
+ * The keys of the object will be ignored.
+ */
+export function addPermissionKeys() {
+    return function (target: any, propertyKey: string) {
+        for (const value of Object.values(flattenObject(target[propertyKey]))) {
+            PermissionManagerService.addPermissionKey(value);
+        }
+    }
+}
