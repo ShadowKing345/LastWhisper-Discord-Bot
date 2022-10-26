4372b90d667f517b4df3642fc9ebaa0b846e2368
alex <alexandruturcanu915@gmail.com>
2022-10-26 09:17:04 +0300

    Added a merger function.

--- src/models/permission_manager/permissionManagerConfig.model.ts
+++ src/models/permission_manager/permissionManagerConfig.model.ts
@@ -1,11 +1,29 @@
 import { IEntity } from "../../utils/objects/repositoryBase.js";
 import { Permission } from "./permission.model.js";
+import { ToJsonBase } from "../../utils/objects/toJsonBase.js";
+import { deepMerge } from "../../utils/index.js";
 
 /**
  * Permission manager configuration object.
  */
-export class PermissionManagerConfig implements IEntity {
+export class PermissionManagerConfig extends ToJsonBase<PermissionManagerConfig> implements IEntity {
     public _id;
     public guildId: string;
     public permissions: { [key: string]: Permission } = {};
+
+    public merge(obj: Partial<PermissionManagerConfig>): PermissionManagerConfig {
+        if (obj._id) {
+            this._id = obj._id;
+        }
+
+        if (obj.guildId) {
+            this.guildId = obj.guildId;
+        }
+
+        if (obj.permissions) {
+            this.permissions = deepMerge(this.permissions ?? {}, obj.permissions);
+        }
+
+        return this;
+    }
 }
