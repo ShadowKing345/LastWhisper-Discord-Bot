95126d18b9231c0f73a6a9a8e24823b631283ab6
alex <alexandruturcanu915@gmail.com>
2022-11-11 15:03:40 +0200

    Added a new kind of decorator. Formatted decorator. Added index.

---
+++ src/utils/decorators/deferReply.ts
@@ -0,0 +1,22 @@
+/**
+ * Defers the interaction response then executes the function.
+ * Caution is advised when using this command.
+ * @param ephemeral Should the messages be hidden or not.
+ */
+import { ChatInputCommandInteraction } from "discord.js";
+
+export function deferReply(ephemeral = false): (target: object, propertyKey: string | symbol, descriptor: PropertyDescriptor) => PropertyDescriptor {
+  return function(_target: object, _propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value as (interaction: ChatInputCommandInteraction, ...args: unknown[]) => Promise<unknown>;
+
+    descriptor.value = async function(interaction: ChatInputCommandInteraction, ...args: unknown[]) {
+      const response = await interaction.deferReply({ ephemeral });
+
+      await originalMethod.apply(this, [interaction, ...args]);
+
+      return response;
+    };
+
+    return descriptor;
+  };
+}
---
+++ src/utils/decorators/index.ts
@@ -0,0 +1,4 @@
+export * from "./addPermissionKeys.js";
+export * from "./authorize.js";
+export * from "./deferReply.js";
+export * from "./registerModule.js";
