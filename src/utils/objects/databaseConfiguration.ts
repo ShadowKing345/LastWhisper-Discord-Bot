7b57e619ec49ed1eddaa46e2bb65fddc50ed6527
alex <alexandruturcanu915@gmail.com>
2022-12-08 21:58:48 +0200

    Added default values.

--- src/utils/objects/databaseConfiguration.ts
+++ src/utils/objects/databaseConfiguration.ts
@@ -1,8 +1,8 @@
 export class DatabaseConfiguration {
-  public username?: string = null;
-  public password?: string = null;
-  public port?: number = null;
-  public database?: string = null;
+  public username?: string = "postgresql";
+  public password?: string = "postgres";
+  public port?: number = 5432;
+  public database?: string = "Bot";
   public logging?: boolean = false;
-  public sync?: boolean = true;
+  public sync?: boolean = false;
 }
