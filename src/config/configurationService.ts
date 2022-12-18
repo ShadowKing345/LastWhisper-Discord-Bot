7f7db7757144df27b03182c87bdb59b7a7cedb5f
alex <alexandruturcanu915@gmail.com>
2022-12-18 14:20:56 +0200

    Added documentation.

--- src/config/configurationService.ts
+++ src/config/configurationService.ts
@@ -3,14 +3,25 @@ import fs from "fs";
 import { flattenObject, deepMerge } from "../utils/index.js";
 import { ProjectConfiguration } from "../utils/objects/index.js";
 
+/**
+ * A Service class used to register configuration objects into the dependency tree.
+ */
 export class ConfigurationService {
   public static readonly configPath = process.env.CONFIG_PATH ?? "./config/default.json";
   public static readonly devConfigPath = process.env.DEV_CONFIG_PATH ?? "./config/development.json";
   private static flattenConfigs: Map<string, unknown> = new Map<string, unknown>();
 
+  /**
+   * Registers a config object with a key to a container.
+   * @param key The key for the config file.
+   * @param entity A constructor function to create an object from.
+   * @param container Override for the global container.
+   * @throws A bad configuration error when the key cannot be found.
+   * @constructor
+   */
   public static RegisterConfiguration<T extends object>(key: string, entity: { new(): T }, container: DependencyContainer = globalContainer): void {
     if (ConfigurationService.flattenConfigs.size < 1) {
-      this.getConfigs();
+      this.createConfigMap();
     }
 
     const map = ConfigurationService.flattenConfigs;
@@ -24,7 +35,12 @@ export class ConfigurationService {
     container.register(entity, { useValue: e });
   }
 
-  private static getConfigs(): void {
+  /**
+   * Creates a configuration map to be used for each configuration service.
+   * This method also clear whatever is currently stored.
+   * @private
+   */
+  private static createConfigMap(): void {
     ConfigurationService.flattenConfigs.clear();
 
     const config = ConfigurationService.parseConfigs();
@@ -36,6 +52,13 @@ export class ConfigurationService {
     ConfigurationService.flattenConfigs.set("", config);
   }
 
+  /**
+   * Attempts to read a file and return its string content.
+   * @param path The path to the file.
+   * @return The string content or null if it cannot be found.
+   * @private
+   */
+
   private static readFile(path: string): string {
     if (!(path && fs.existsSync(path))) {
       return null;
@@ -43,6 +66,14 @@ export class ConfigurationService {
 
     return fs.readFileSync(path, "utf-8");
   }
+
+  /**
+   * Creates a configuration object given a path.
+   * Only works with JSON objects.
+   * @param path The file path of the configuration.
+   * @return A ProjectConfiguration object or null.
+   * @private
+   */
 
   private static parseConfigFile(path: string): ProjectConfiguration {
     const objStr: string = ConfigurationService.readFile(path);
@@ -54,11 +85,16 @@ export class ConfigurationService {
     return deepMerge(ProjectConfiguration, JSON.parse(objStr));
   }
 
+  /**
+   * Returns a ProjectConfiguration object.
+   * This function also merges the relevant config file for each environment.
+   * @private
+   */
   private static parseConfigs(): ProjectConfiguration {
     const config = ConfigurationService.parseConfigFile(ConfigurationService.configPath);
 
     if (process.env.NODE_ENV !== "development") {
-      return config;
+      return config ?? new ProjectConfiguration();
     }
 
     const devConfig = ConfigurationService.parseConfigFile(ConfigurationService.devConfigPath);
