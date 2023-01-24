5c2f8450911de6e574a9bc77303492f1491d2c59
ShadowKing345 <alexandruturcanu915@gmail.com>
2023-01-24 21:09:58 +0200

    Added an index file.

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+export * from "./entities/index.js";
+export * from "./configurationKeys.js";
+export * from "./databaseService.js";
+export * from "./logger.js";
+export * from "./moduleService.js";
