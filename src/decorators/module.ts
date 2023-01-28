86554fb72f33106eb6657195a06ccba02dba19b8
ShadowKing345 <alexandruturcanu915@gmail.com>
2023-01-29 01:35:58 +0200

    Removed implied return type.

--- src/decorators/module.ts
+++ src/decorators/module.ts
@@ -5,7 +5,7 @@ import { Module } from "../modules/module.js";
 /**
  * Decorator that registers a module base class as a singleton of its own type and of token ModuleBase.name.
  */
-export function module<T extends Module>(): (target: constructor<T>) => void {
+export function module<T extends Module>() {
   return function (target: constructor<T>) {
     injectable()(target);
     container.registerSingleton<T>(target);
