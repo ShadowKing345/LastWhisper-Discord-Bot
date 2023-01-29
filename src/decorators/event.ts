5e32d27ef6b9d1e7659f8db96ee832475b32d7eb
alex <alexandruturcanu915@gmail.com>
2023-01-29 15:31:41 +0200

    Added a register event decorator.

---
+++ src/decorators/event.ts
@@ -0,0 +1,20 @@
+import { ModuleService } from "../config/index.js";
+import { EventListener, Bot } from "../objects/index.js";
+import { CTR } from "../utils/commonTypes.js";
+import { Module } from "../modules/module.js";
+import { ClientEvents } from "discord.js";
+
+/**
+ * Decorator that attempts to register commands to the module service system.
+ * Methods decorated with this command act as the executed method.
+ */
+export function Event<T extends Module, K extends keyof ClientEvents>(event: K) {
+  return function(target: unknown, _: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+    ModuleService.registerEventListener(new EventListener(
+      event,
+      descriptor.value as (client: Bot, args: ClientEvents[K]) => Promise<void>,
+    ), target.constructor as CTR<T>);
+
+    return descriptor;
+  };
+}
