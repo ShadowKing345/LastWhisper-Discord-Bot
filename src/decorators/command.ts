4d6ffe948046bbc13bfc26639c9d7b036955518d
ShadowKing345 <alexandruturcanu915@gmail.com>
2023-01-29 02:05:14 +0200

    Cleaned up and changed what is passed to the module service register command function.

--- src/decorators/command.ts
+++ src/decorators/command.ts
@@ -2,22 +2,21 @@ import { ModuleService } from "../config/index.js";
 import { SlashCommand } from "../objects/index.js";
 import { ChatInputCommandInteraction } from "discord.js";
 import { Module } from "../modules/module.js";
+import { CTR } from "../utils/commonTypes.js";
 
 /**
  * Decorator that attempts to register commands to the module service system.
  * Methods decorated with this command act as the executed method.
  */
 export function Command<T extends Module>(command: Partial<Omit<SlashCommand, "callback">>) {
-  return function(target: T, _: string, descriptor: PropertyDescriptor): PropertyDescriptor {
-    const value = descriptor.value as (interaction: ChatInputCommandInteraction) => Promise<unknown>;
-
+  return function(target: unknown, _: string, descriptor: PropertyDescriptor): PropertyDescriptor {
     ModuleService.registerCommand(new SlashCommand({
       name: command.name,
       description: command.description,
-      callback: value,
+      callback: descriptor.value as (interaction: ChatInputCommandInteraction) => Promise<unknown>,
       subcommands: command.subcommands,
       options: command.options,
-    }), target.constructor.name);
+    }), target as CTR<T>);
 
     return descriptor;
   };
