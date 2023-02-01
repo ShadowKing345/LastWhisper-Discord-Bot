a5ae14b7a7315b2117c6056f744173ae8e84df81
ShadowKing345 <alexandruturcanu915@gmail.com>
2023-02-01 22:57:07 +0200

    Cleaned the returned type as it was inferred.

--- src/decorators/authorize.ts
+++ src/decorators/authorize.ts
@@ -6,16 +6,14 @@ import { Module } from "../modules/module.js";
  * If the permission manager service is missing the function is called as if nothing happened.
  * @param key Names of the permission check key.
  */
-export function authorize<T extends Module>(
-  key: string,
-): (target: T, propertyKey: string | symbol, descriptor: PropertyDescriptor) => PropertyDescriptor {
-  return function (_target: T, _propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+export function authorize<T extends Module>(key: string) {
+  return function(_target: T, _propertyKey: string | symbol, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value as (
       interaction: ChatInputCommandInteraction,
       ...args: unknown[]
     ) => Promise<unknown>;
 
-    descriptor.value = async function (interaction: ChatInputCommandInteraction, ...args: unknown[]): Promise<unknown> {
+    descriptor.value = async function(interaction: ChatInputCommandInteraction, ...args: unknown[]): Promise<unknown> {
       if (
         (this as T).permissionManagerService &&
         !(await (this as T).permissionManagerService.isAuthorized(interaction, key))
@@ -27,9 +25,9 @@ export function authorize<T extends Module>(
 
         return interaction.isRepliable()
           ? interaction.reply({
-              content: "Sorry you do not have the permissions to use this command.",
-              ephemeral: true,
-            })
+            content: "Sorry you do not have the permissions to use this command.",
+            ephemeral: true,
+          })
           : null;
       }
 
