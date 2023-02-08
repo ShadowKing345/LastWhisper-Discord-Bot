b4f4147658f97a14acb87f6e09e386337c2c30ea
ShadowKing345 <alexandruturcanu915@gmail.com>
2023-02-08 21:21:37 +0200

    Set database configuration to permanently be postgresql.

--- src/config/entities/databaseConfiguration.ts
+++ src/config/entities/databaseConfiguration.ts
@@ -1,10 +1,10 @@
-import { DatabaseType } from "typeorm";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions.js";
 
-export class DatabaseConfiguration {
-    public type: DatabaseType = "postgres";
-    public username?: string = "postgresql";
-    public password?: string = "postgresql";
-    public port?: number = 5432;
-    public database?: string = "Bot";
-    public logging?: boolean = false;
+export class DatabaseConfiguration implements PostgresConnectionOptions {
+  public readonly type = "postgres";
+  public username?: string = "postgresql";
+  public password?: string = "postgresql";
+  public port?: number = 5432;
+  public database?: string = "Bot";
+  public logging?: boolean = false;
 }
