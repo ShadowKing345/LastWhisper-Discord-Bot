0b629ce3f326abd44cbcfce2fae900d98e1a4219
alex <alexandruturcanu915@gmail.com>
2023-02-19 21:21:53 +0200

    added hostname for database.

--- src/config/entities/databaseConfiguration.ts
+++ src/config/entities/databaseConfiguration.ts
@@ -4,6 +4,7 @@ export class DatabaseConfiguration implements PostgresConnectionOptions {
   public readonly type = "postgres";
   public username?: string = "postgresql";
   public password?: string = "postgresql";
+  public host?: string = "127.0.0.1";
   public port?: number = 5432;
   public database?: string = "Bot";
   public logging?: boolean = false;
