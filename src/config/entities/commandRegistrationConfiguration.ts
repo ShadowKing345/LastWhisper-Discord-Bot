dc464ea33f02fa16067a0c66accb4efb4c5aea00
Alexandru Turcanu <alexandruturcanu915@gmail.com>
2023-03-12 20:00:08 +0200

    Improve command manager script (#22)
    * Fixed issue were slash command manager was only registering black listed items.
    
    * Better defined arguments.
    
    * Made command object properly have an action.
    
    * Added arguments for commandManager to have a sub command instead of a flag.
    
    * Removed the unregister flag and made it a function parameter.
    
    * Cleaned up console.log messages.

--- src/config/entities/commandRegistrationConfiguration.ts
+++ src/config/entities/commandRegistrationConfiguration.ts
@@ -2,7 +2,6 @@ export class CommandRegistrationConfiguration {
     public clientId: string = null;
     public guildId: string = null;
     public registerForGuild?: boolean = false;
-    public unregister?: boolean = false;
 
     public get isValid(): boolean {
         if( !this.clientId ) {
@@ -14,5 +13,21 @@ export class CommandRegistrationConfiguration {
         }
 
         return true;
+    }
+
+    public merge( obj: Partial<CommandRegistrationConfiguration> ): this {
+        if( obj.clientId && typeof obj.clientId === "string" ) {
+            this.clientId = obj.clientId;
+        }
+
+        if( obj.guildId && typeof obj.guildId === "string" ) {
+            this.guildId = obj.guildId;
+        }
+
+        if( typeof obj.registerForGuild === "boolean" ) {
+            this.registerForGuild = obj.registerForGuild;
+        }
+        
+        return this;
     }
 }
