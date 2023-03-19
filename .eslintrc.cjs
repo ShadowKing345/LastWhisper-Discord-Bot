43e63ae979d8d38fef6b1aa3250a7e51b77cf274
alex <alexandruturcanu915@gmail.com>
2023-03-19 15:43:44 +0200

    Removed complex code.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,7 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: [ "@typescript-eslint" ],
     parserOptions: {
-        project: [ './tsconfig.json' ],
+        project: [ './tsconfig.test.json' ],
         tsconfigRootDir: __dirname,
         ecmaVersion: "latest",
         sourceType: "module"
--- src/tap.test.ts
+++ src/tap.test.ts
@@ -1,22 +1,3 @@
-import tap from "tap";
-
-function myAwesomeFunction(x: number): string {
-  if(x % 2 === 0) {
-    return "even";
-  } else if(x % 2 === 1) {
-    return "odd";
-  } else if(x > 100) {
-    return "big";
-  } else if(x < 0) {
-    return "negative";
-  }
-
-  throw new Error();
-}
+import * as tap from "tap";
 
 tap.pass("Hello World");
-
-tap.equal(myAwesomeFunction(1), "odd");
-tap.equal(myAwesomeFunction(2), "even");
-tap.equal(myAwesomeFunction(200), "big");
-tap.equal(myAwesomeFunction(-10), "negative");
