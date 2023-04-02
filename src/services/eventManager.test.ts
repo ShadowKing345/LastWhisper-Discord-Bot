529303513def90153ac42f96ce4ff10a203e6215
alex <alexandruturcanu915@gmail.com>
2023-04-02 13:12:00 +0300

    Cleaned up test code.

--- src/services/eventManager.test.ts
+++ src/services/eventManager.test.ts
@@ -1,20 +1,20 @@
 import tap from "tap";
-import { EventManagerSettingsRepository } from "../repositories/eventManager/eventManagerSettingsRepository.js";
-import { EventManagerSettings } from "../entities/eventManager/index.js";
-import { EventManagerService } from "./eventManager.js";
-import { EventObjectRepository } from "../repositories/eventManager/eventObjectRepository.js";
-import { EventReminderRepository } from "../repositories/eventManager/eventReminderRepository.js";
-import { DateTime } from "luxon";
+import {EventManagerSettingsRepository} from "../repositories/eventManager/eventManagerSettingsRepository.js";
+import {EventManagerSettings} from "../entities/eventManager/index.js";
+import {EventManagerService} from "./eventManager.js";
+import {EventObjectRepository} from "../repositories/eventManager/eventObjectRepository.js";
+import {EventReminderRepository} from "../repositories/eventManager/eventReminderRepository.js";
+import {DateTime} from "luxon";
 
 class MockSettingRepo extends EventManagerSettingsRepository {
     private readonly settings: EventManagerSettings;
 
     constructor() {
-        super( null );
+        super(null);
 
         const settings = new EventManagerSettings();
         settings.guildId = "0";
-        settings.delimiterCharacters = [ "[", "]" ];
+        settings.delimiterCharacters = ["[", "]"];
         settings.announcement = "announcement";
         settings.description = "description";
         settings.dateTime = "time";
@@ -24,43 +24,44 @@ class MockSettingRepo extends EventManagerSettingsRepository {
 
 
     public async findOneOrCreateByGuildId(): Promise<EventManagerSettings> {
-        return Promise.resolve( this.settings );
+        return Promise.resolve(this.settings);
     }
 }
 
 class MockObjectRepo extends EventObjectRepository {
     constructor() {
-        super( null );
+        super(null);
     }
 }
 
 class MockReminderRepo extends EventReminderRepository {
     constructor() {
-        super( null );
+        super(null);
     }
 }
 
-tap.test( "Testing the parser.", async t => {
-    await t.test( "Message got parsed correctly.", async t => {
-        const service = new EventManagerService( new MockSettingRepo(), new MockObjectRepo(), new MockReminderRepo() );
+tap.test("Testing the parser.", async t => {
+    await t.test("Message got parsed correctly.", async t => {
+        const service = new EventManagerService(new MockSettingRepo(), new MockObjectRepo(), new MockReminderRepo());
 
-        const result = await service.parseEvent( "0", `[announcement]\nHello World\n[description]\nTesting\n[time]\n<t:${ DateTime.now().plus( { days: 1 } ).toUnixInteger() }:f>` );
-        t.ok( result );
-        t.ok( result?.isValid );
-
-        t.end();
-    } );
-
-    await t.test( "Message is wrong", async t => {
-        const service = new EventManagerService( new MockSettingRepo(), new MockObjectRepo(), new MockReminderRepo() );
-
-        t.notOk( ( await service.parseEvent( "0", `[description]\nTesting\n[time]\n<t:${ DateTime.now().plus( { days: 1 } ).toUnixInteger() }:f>` ) ).isValid, "Bad event name" )
-        t.notOk( ( await service.parseEvent( "0", `[announcement]\nHello World\n[time]\n<t:${ DateTime.now().plus( { days: 1 } ).toUnixInteger() }:f>` ) ).isValid, "Bad event description" )
-        t.notOk( ( await service.parseEvent( "0", `[announcement]\nHello World\n[description]\nTesting` ) ).isValid, "Bad time" )
-        t.notOk( ( await service.parseEvent( "0", `[announcement]\nHello World\n[description]\nTesting\n[time]\n<t:${ DateTime.now().toUnixInteger() }:f>` ) ).isValid, "Time is before now" );
+        const result = await service.parseEvent("0", `[announcement]\nHello World\n[description]\nTesting\n[time]\n<t:${DateTime.now().plus({days: 1}).toUnixInteger()}:f>`);
+        t.ok(result);
+        t.ok(result?.isValid);
 
         t.end();
-    } );
+    });
+
+    await t.test("Message is wrong", async t => {
+        const service = new EventManagerService(new MockSettingRepo(), new MockObjectRepo(), new MockReminderRepo());
+        const now = DateTime.now().plus({days: 1}).toUnixInteger();
+
+        t.notOk((await service.parseEvent("0", `[description]\nTesting\n[time]\n<t:${now}:f>`)).isValid, "Bad event name")
+        t.notOk((await service.parseEvent("0", `[announcement]\nHello World\n[time]\n<t:${now}:f>`)).isValid, "Bad event description")
+        t.notOk((await service.parseEvent("0", `[announcement]\nHello World\n[description]\nTesting`)).isValid, "Bad time")
+        t.notOk((await service.parseEvent("0", `[announcement]\nHello World\n[description]\nTesting\n[time]\n<t:${0}:f>`)).isValid, "Time is before now");
+
+        t.end();
+    });
 
     t.end();
-} ).catch( console.error );
+}).catch(console.error);
