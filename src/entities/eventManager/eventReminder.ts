fa7333cf4f05f4198e526e6dfae948c708998c9b
alex <alexandruturcanu915@gmail.com>
2023-04-22 13:55:44 +0300

    Reworked how the constructor handles data.

--- src/entities/eventManager/eventReminder.ts
+++ src/entities/eventManager/eventReminder.ts
@@ -1,6 +1,7 @@
-import { Duration, DateTime } from "luxon";
-import { Entity, Column } from "typeorm";
-import { EntityBase } from "../entityBase.js";
+import {Duration, DateTime} from "luxon";
+import {Entity, Column} from "typeorm";
+import {EntityBase} from "../entityBase.js";
+import {isStringNullOrEmpty} from "../../utils/index.js";
 
 /**
  * Reminder trigger. Used to calculate how long until an event reminder needs to be sent.
@@ -14,18 +15,34 @@ export class EventReminder extends EntityBase {
     @Column()
     public timeDelta: string = null;
 
-    constructor() {
+    constructor(data: Partial<EventReminder> = null) {
         super();
+
+        if (data) {
+            this.merge(data);
+        }
+    }
+
+    public merge(obj: Partial<EventReminder>): EventReminder {
+        if (!isStringNullOrEmpty(obj.message)) {
+            this.message = obj.message;
+        }
+
+        if (!isStringNullOrEmpty(obj.timeDelta)) {
+            this.timeDelta = obj.timeDelta;
+        }
+
+        return this;
     }
 
     /**
      * Returns the duration equivalent of the time delta variable.
      */
     public get asDuration(): Duration {
-        const hold = DateTime.fromFormat( this.timeDelta, "HH:mm" );
-        return Duration.fromObject( {
-            hours: hold.get( "hour" ),
-            minutes: hold.get( "minute" ),
-        } );
+        const hold = DateTime.fromFormat(this.timeDelta, "HH:mm");
+        return Duration.fromObject({
+            hours: hold.get("hour"),
+            minutes: hold.get("minute"),
+        });
     }
 }
