23b106eab9b80c0fb41255fea55a0efc256ab484
ShadowKing345 <alexandruturcanu915@gmail.com>
2023-05-13 01:26:36 +0300

    Added merge function for module configuration.

--- src/config/entities/moduleConfiguration.ts
+++ src/config/entities/moduleConfiguration.ts
@@ -1,3 +1,5 @@
+import { isArray } from "../../utils/index.js";
+
 /**
  * Configuration object for the module configuration service.
  */
@@ -15,4 +17,32 @@ export class ModuleConfiguration {
     public modules?: string[] = [ "DevModule" ];
     // Should the list be treated as a blacklist.
     public blacklist?: boolean = true;
+
+    public merge( obj: Partial<ModuleConfiguration> ): ModuleConfiguration {
+        if( obj.enableCommands !== undefined ) {
+            this.enableCommands = obj.enableCommands;
+        }
+
+        if( obj.enableEventListeners !== undefined ) {
+            this.enableEventListeners = obj.enableEventListeners;
+        }
+
+        if( obj.enableTimers !== undefined ) {
+            this.enableTimers = obj.enableTimers;
+        }
+
+        if( obj.enableInteractions !== undefined ) {
+            this.enableInteractions = obj.enableInteractions;
+        }
+
+        if( isArray( obj.modules ) ) {
+            this.modules = obj.modules.filter( item => typeof item === "string" );
+        }
+
+        if( obj.blacklist !== undefined ) {
+            this.blacklist = obj.blacklist;
+        }
+
+        return this;
+    }
 }
